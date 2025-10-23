76feb5ce87fefb2e67001cdf4860ded5efb63558
John Stultz <jstultz@google.com>
2025-10-23 23:09:26 +0000

    udelay race window opener

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1198,6 +1198,7 @@ s64 update_curr_common(struct rq *rq)
 {
 	return update_se(rq, &rq->donor->se);
 }
+#include <linux/delay.h>
 
 /*
  * Update the current task's runtime statistics.
@@ -1218,6 +1219,7 @@ static void update_curr(struct cfs_rq *cfs_rq)
 	if (unlikely(!curr))
 		return;
 
+	udelay(cpu_of(rq)*10);
 	delta_exec = update_se(rq, curr);
 	if (unlikely(delta_exec <= 0))
 		return;
