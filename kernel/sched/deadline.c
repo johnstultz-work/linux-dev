2df88f65d0ea0ecf6864476df37083f7c0531798
Christian Loehle <christian.loehle@arm.com>
2026-07-07 18:45:57 +0100

    NEEDSWORK: sched/deadline: Ignore proxy-exec sched_yield()
    With proxy execution, rq->curr is the execution context while rq->donor is
    the donating context. rq->curr's sched_yield() is dispatched through
    the donor class so that proxy execution follows the effective scheduling
    context.
    
    For SCHED_DEADLINE, this is too strong. yield_task_dl() does not just ask
    for another task of equal priority to get to run, it marks the current DL
    entity as yielded and forces it to sleep until replenishment. These
    yield semantics are fundamentally different from FIFO/RR (where if no
    equal-priority tasks are runnable, no harm done, they get picked again
    immediately) or OTHER (also doesn't cause priority inversion), so do not
    mix these semantics by ignoring a sched_yield() on DL donors.
    
    NOTE: Peter objected to just ignoring the yield and suggested it be
    queued on the proxy for after the donation is up. Almost seems like
    we might be able to set dl_yielded on the rq->curr and then return
    in the proxy case, but I need to read through the paths more
    
    Fixes: 127b90315ca0 ("sched/proxy: Yield the donor task")
    Acked-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Christian Loehle <christian.loehle@arm.com>
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2143,6 +2143,9 @@ static bool dequeue_task_dl(struct rq *rq, struct task_struct *p, int flags)
  */
 static void yield_task_dl(struct rq *rq)
 {
+	if (sched_proxy_exec() && rq->curr != rq->donor)
+		return;
+
 	/*
 	 * We make the task go to sleep until its current deadline by
 	 * forcing its runtime to zero. This way, update_curr_dl() stops
